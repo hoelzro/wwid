13bb1aa0826272d3e7d5dd5f1698ef28e4a23e1b
Rob Hoelz <rob@hoelz.ro>
2014-07-23 07:46:01 -0500

    Add docs for commands

--- lib/WWID/CLI.pm
+++ lib/WWID/CLI.pm
@@ -5,16 +5,20 @@ my class Effort {
 }
 
 class WWID::CLI does Subcommander::Application {
+    #| Creates a new task
     method new_task(Str :$name!, Effort :$effort!) is subcommand('new') {
     }
 
+    #| Asks wwid for something new to work on
     method next_task() is subcommand('next') {
         say '*** STUB next task ***';
     }
 
+    #| Mark a task as completed
     method complete(Str :$name!) is subcommand {
     }
 
+    #| Recalculates the value for tasks that need it
     method recalcuate() is subcommand {
     }
 }
